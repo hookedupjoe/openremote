379b04672ff3b9f4fd7b7fb7ddafe487c6ea89fb
Joseph Francis <joefran@us.ibm.com>
2018-02-17 13:53:16 -0500

    Initial Updates - Icons, config, demo app

--- OpenRemoteApp/www/js/index.js
+++ OpenRemoteApp/www/js/index.js
@@ -1,46 +1,92 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+(function(){
+    //--- Start Bubble
+    
+    /*
+     * Licensed to the Apache Software Foundation (ASF) under one
+     * or more contributor license agreements.  See the NOTICE file
+     * distributed with this work for additional information
+     * regarding copyright ownership.  The ASF licenses this file
+     * to you under the Apache License, Version 2.0 (the
+     * "License"); you may not use this file except in compliance
+     * with the License.  You may obtain a copy of the License at
+     *
+     * http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing,
+     * software distributed under the License is distributed on an
+     * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+     * KIND, either express or implied.  See the License for the
+     * specific language governing permissions and limitations
+     * under the License.
+     */
+    
+     
+     var app = {
+        // Application Constructor
+        initialize: function() {
+            document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        },
+    
+        // deviceready Event Handler
+        //
+        // Bind any cordova events here. Common events are:
+        // 'pause', 'resume', etc.
+        onDeviceReady: function() {
+            this.receivedEvent('deviceready');
+            setup();
+        },
+        // Update DOM on a Received Event
+        receivedEvent: function(id) {
+            var parentElement = document.getElementById(id);
+            var listeningElement = parentElement.querySelector('.listening');
+            var receivedElement = parentElement.querySelector('.received');
+    
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+    
+            console.log('Received Event: ' + id);
+        }
+    };
+    
+    app.initialize();
+    
+    //---- general variables
+    var store,
+        btnUpdateName,
+        fldName,
+        showName
+    ;
+    //---- End general variables
+    
+    function setup(){
+        setupBindings();
+        refreshFromStore();
+        showName.innerHTML = "Started";
     }
-};
-
-app.initialize();
+    
+    function onBtnNameUpdateClick(){
+        console.log('onBtnNameUpdateClick');
+        showName.innerHTML = 'Testing';
+        localStorage.setItem("Name", fldName.value || '');
+        refreshFromStore();
+    }
+    
+    function setupBindings(){
+        fldName = document.getElementById("fldName");
+        showName = document.getElementById("showName");
+        btnUpdateName = document.getElementById("btnUpdateName");
+        btnUpdateName.addEventListener("click", onBtnNameUpdateClick);  
+    }
+    
+    function refreshFromStore(){
+        var tmpName = localStorage.getItem("Name") || '';
+        fldName.value = tmpName;
+        showName.innerHTML = tmpName;
+    }
+    
+    function getLocalStorageByKey() {
+        return (localStorage.key(0));
+    }
+    
+    //--- End Bubble 
+    })();
